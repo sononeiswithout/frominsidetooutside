23038f79c8eff8ae7c363de4782dd7c8de7ab3ed
sononeiswithout <32646482+sononeiswithout@users.noreply.github.com>
2024-12-18 02:39:00 +0100

    Create index.js
    Add an index.js File: This file will act as the home page of your application. Create pages/index.js with the following content:

---
+++ nft-wikipedia-directory/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to NFT Wikipedia Directory</h1>
+      <p>Create, rate, and explore NFT-backed knowledge contributions.</p>
+    </div>
+  );
+}
